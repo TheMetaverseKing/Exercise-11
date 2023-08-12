d39f413a11a17f4f70bfdf2dca6de65588c33fb0
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-13 00:52:37 +0500

    Add files via upload

---
+++ Ex11.js
@@ -0,0 +1,7 @@
+"use strict";
+// Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var bestfriend = ["Warren Buffet", "Jeff Bezos", "Christiano Ronaldo", "Quaid e Azam"];
+console.log(bestfriend[0]);
+console.log(bestfriend[1]);
+console.log(bestfriend[2]);
+console.log(bestfriend[3]);
---
+++ Ex11.ts
@@ -0,0 +1,9 @@
+// Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+
+var bestfriend:string []=["Warren Buffet", "Jeff Bezos", "Christiano Ronaldo", "Quaid e Azam"]
+console.log(bestfriend[0])
+console.log(bestfriend[1])
+console.log(bestfriend[2])
+console.log(bestfriend[3])
